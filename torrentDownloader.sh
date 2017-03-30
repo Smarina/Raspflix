3d4f1338be54a800a22461e762a3e6c1ffaeac4c
Sergio <smarinad93@gmail.com>
2017-03-30 20:17:46 +0200

    Avoid downloading the same movie by checking its torrent-backup first

--- torrentDownloader.sh
+++ torrentDownloader.sh
@@ -1,2 +1,6 @@
-cd /mnt/moviedrive/deluge/watch
-curl -o "$2.torrent" $1
+cd /mnt/moviedrive/deluge
+if [ ! -f "./torrent-backup/$2.torrent" ]; then
+    curl -o "./watch/$2.torrent" $1
+else
+	echo "$2 has been already downloaded"
+fi
